4152989ee64abbff34326685e45f887a2f106092
LinkinGeorge <georgdonner98@gmail.com>
2017-10-15 01:21:36 +0200

    Add router.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Dashboard from './containers/Dashboard';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <Router>
+    <div>
+      <Route path='/:notebook/' component={Dashboard} />
+    </div>
+  </Router>, 
+  document.getElementById('root')
+);
