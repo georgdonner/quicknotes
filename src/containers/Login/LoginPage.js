65747c23d82e253e76ebe1423e51a965437f4ea9
Georg Donner <georgdonner98@gmail.com>
2017-11-05 12:29:23 +0100

    Style login page.

--- src/containers/Login/LoginPage.js
+++ src/containers/Login/LoginPage.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import './LoginPage.css';
 
 const LoginPage = () =>
   (
-    <div>
-      <a href="/api/auth">Login with Github</a>
+    <div className=" hero is-fullheight login-page">
+      <div className="hero-body">
+        <div className="container has-text-centered">
+          <h1 className="title" id="main-title">QUICKNOTES</h1>
+          <div className="login-form">
+            <h2 className="title">Sign In</h2>
+            <a href="/api/auth" className="button is-medium github-button">
+              <span className="icon is-medium">
+                <i className="fa fa-github" />
+              </span>
+              <span>Sign in with GitHub</span>
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 
