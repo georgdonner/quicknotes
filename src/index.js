abd4ef7e4d1a8809bb0413957c4c5fa2db977d29
Georg Donner <georgdonner98@gmail.com>
2017-12-03 16:37:21 +0100

    Add redux-thunk middleware.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import axios from 'axios';
 import 'react-toastify/dist/ReactToastify.min.css';
@@ -11,7 +12,10 @@ import rootReducer from './store/reducers';
 
 axios.defaults.withCredentials = true;
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk),
+);
 
 ReactDOM.render(
   <Provider store={store}><App /></Provider>,
