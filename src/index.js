95cd77d43c89dc64e4e50727725670f1d0c17c79
Georg Donner <georgdonner98@gmail.com>
2017-12-04 20:45:55 +0100

    Set up redux dev tools.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import axios from 'axios';
@@ -13,9 +13,11 @@ import { fetchUserAndNotebooks } from './store/actions';
 
 axios.defaults.withCredentials = true;
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
   rootReducer,
-  applyMiddleware(thunk),
+  composeEnhancers(applyMiddleware(thunk)),
 );
 
 store.dispatch(fetchUserAndNotebooks());
